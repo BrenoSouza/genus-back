2a95ff77e94838007fe76a212ec40ec7fc76cbf6
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-09-05 01:22:34 -0300

    Start project

---
+++ src/main/java/br/edu/ufcg/genus/GenusApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ufcg.genus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenusApplication.class, args);
+	}
+}
---
+++ src/test/java/br/edu/ufcg/genus/GenusApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ufcg.genus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GenusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
