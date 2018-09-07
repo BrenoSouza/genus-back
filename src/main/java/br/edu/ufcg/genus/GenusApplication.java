402a616c5ecb3e65e5b70b19fefea9d73f07d7b9
Iaron Araujo <iaron.araujo@ccc.ufcg.edu.br>
2018-09-07 12:32:21 -0300

    The first query is working now.

--- src/main/java/br/edu/ufcg/genus/GenusApplication.java
+++ src/main/java/br/edu/ufcg/genus/GenusApplication.java
@@ -2,11 +2,19 @@ package br.edu.ufcg.genus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.edu.ufcg.genus.dummy.DummyQueries;
 
 @SpringBootApplication
 public class GenusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenusApplication.class, args);
+	}
+	
+	@Bean
+	public DummyQueries dummyQueries() {
+		return new DummyQueries();
 	}
 }
