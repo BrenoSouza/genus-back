4dd27af540c9800855a64d066cc055c6fdfecb2c
Iaron Araujo <iaron.araujo@ccc.ufcg.edu.br>
2018-09-26 10:17:19 -0300

    Small Refactoring and Implemented find queries for Grade and Subject

--- src/main/java/br/edu/ufcg/genus/graphql/queries/UserQueries.java
+++ src/main/java/br/edu/ufcg/genus/graphql/queries/UserQueries.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
+import br.edu.ufcg.genus.exception.InvalidIDException;
 import br.edu.ufcg.genus.inputs.AuthenticationInput;
 import br.edu.ufcg.genus.models.User;
 import br.edu.ufcg.genus.models.UserRole;
@@ -12,14 +13,14 @@ import br.edu.ufcg.genus.services.UserService;
 public class UserQueries implements GraphQLQueryResolver {
 	
 	@Autowired
-	UserService userService;
+	private UserService userService;
 	
 	public String login (AuthenticationInput input) {
 		return userService.login(input);		
 	}
 
     public User findUser(long id) {
-        return userService.findUserById(id).orElse(null);
+        return userService.findUserById(id).orElseThrow(() -> new InvalidIDException());
     }
 
     public User findLoggedUser() {
