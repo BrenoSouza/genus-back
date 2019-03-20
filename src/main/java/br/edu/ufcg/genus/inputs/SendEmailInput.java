6e2988f725b0d1ff288acae501ec5487b0e025ae
iaronaraujo <iaron.araujo@ccc.ufcg.edu.br>
2019-03-20 12:26:36 -0300

    Implemented send mail queries

---
+++ src/main/java/br/edu/ufcg/genus/inputs/SendEmailInput.java
@@ -0,0 +1,67 @@
+package br.edu.ufcg.genus.inputs;
+
+public class SendEmailInput {
+	
+	private String subject;
+	
+	private String text;
+	
+	public SendEmailInput() {
+		
+	}
+	
+	public SendEmailInput(String subject, String text) {
+		this.subject = subject;
+		this.text = text;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SendEmailInput other = (SendEmailInput) obj;
+		if (subject == null) {
+			if (other.subject != null)
+				return false;
+		} else if (!subject.equals(other.subject))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+	
+	
+
+}
--- src/main/java/br/edu/ufcg/genus/utils/PermissionChecker.java
+++ src/main/java/br/edu/ufcg/genus/utils/PermissionChecker.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import br.edu.ufcg.genus.exception.NotAuthorizedException;
 import br.edu.ufcg.genus.models.Discussion;
+import br.edu.ufcg.genus.models.Grade;
 import br.edu.ufcg.genus.models.Reply;
 import br.edu.ufcg.genus.models.Subject;
 import br.edu.ufcg.genus.models.User;
@@ -16,6 +17,16 @@ public class PermissionChecker {
 		if (!permitedRoles.contains(user.getRole(institutionId))) {
 			throw new NotAuthorizedException();
 		}
+	}
+	
+	public static void checkSendMailToGradePermission(User user, Grade grade) {
+		UserRole role = user.getRole(grade.getInstitution().getId());
+		if (!(role.equals(UserRole.ADMIN) || grade.getTeachers().containsKey(user))) throw new NotAuthorizedException();
+	}
+	
+	public static void checkSendMailToSubjectPermission(User user, Subject subject) {
+		UserRole role = user.getRole(subject.getGrade().getInstitution().getId());
+		if (!(role.equals(UserRole.ADMIN) || subject.getTeachers().contains(user))) throw new NotAuthorizedException();
 	}
 	
 	public static void checkSubjectPermission(User user, Subject subject) {
